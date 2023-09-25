fb5ba67a122ac9b2c09c254c6066904eef3c19fc
Mahbubullah <mahbubullahpathan@gmail.com>
2023-09-25 21:55:09 +0600

    search data display

--- src/component/home.jsx
+++ src/component/home.jsx
@@ -1,15 +1,68 @@
 import { useLoaderData } from "react-router-dom";
 import Banner from "./banner";
 import Cards from "./cards";
+import { useState } from "react";
 
 
 const Home = () => {
+    
     const cardsData = useLoaderData();
+    const [filterData, setFilterData ] = useState( [])
+
+    const [inputText, setinputText] = useState('');
+    const [search, isSearch] = useState(false);
+    console.log(search);
+  
+    const handleSearch = () =>{
+        isSearch(true);
+      const searchData = cardsData.filter(data => data.category == inputText )
+   setFilterData(searchData);
+  console.log(searchData);
+    }
+  
+  
+    const handleChange = event => {
+      setinputText(event.target.value);
+  
+     
+    };
+    
+
+
  
     return (
         <div>
-           <Banner></Banner>
-           <Cards cardsData={cardsData}></Cards>
+           <div
+        className="hero min-h-[45vh]"
+        style={{
+          backgroundImage:
+            "url(https://i.ibb.co/RvcBPW0/banner.jpg)",
+        }}
+      >
+        <div className="hero-overlay bg-[#FFFFFFF2] bg-opacity-60"></div>
+        <div className="hero-content text-center text-neutral-content">
+        <div className=" text-center  ">
+        <h2 className="text-4xl text-[#0B0B0B] font-semibold">
+          I Grow By Helping People In Need
+        </h2>
+        <div className="mt-4">
+          <input
+            className="  text-black pl-2 border border-solid border-[#DEDEDE] py-2 rounded-l-lg w-[270px] "
+            type="text"
+            onChange={handleChange}
+            placeholder="Search here...."
+          />
+          <button onClick={handleSearch} className=" bg-[#FF444A] text-white py-2 px-4 rounded-r-lg ">
+            Search
+          </button>
+        </div>
+      </div>
+        </div>
+      </div>
+           {
+            search ? <Cards cardsData={filterData}></Cards>
+            : <Cards cardsData={cardsData}></Cards>
+           }
         </div>
     );
 };
